1145ab58c0152bf4114c5ac3bfa63c119f4afb9a
Huy Hoang <boydaihungst@gmail.com>
2020-06-07 00:59:22 +0700

    fix-destructuring-options

--- src/utils/createEndpoints.ts
+++ src/utils/createEndpoints.ts
@@ -1,69 +1,65 @@
 /* eslint-disable @typescript-eslint/no-dynamic-delete */
 /* eslint-disable dot-notation */
 /* eslint-disable camelcase */
-import { URLSearchParams } from 'url';
+import { URLSearchParams } from "url";
 
-import { Response } from 'got';
+import { Response } from "got";
 
 import {
   CreatedRequestOptions,
   RequestType,
   RequestOption,
-} from './interfaces';
-import parseEndpoint from './parseEndpoint';
-import normalizeQuery from './normalizeQuery';
-import request from './requests';
-import catchHTTPErrors from './catchHTTPErrors';
-import DataController from '../DataController';
+} from "./interfaces";
+import parseEndpoint from "./parseEndpoint";
+import normalizeQuery from "./normalizeQuery";
+import request from "./requests";
+import catchHTTPErrors from "./catchHTTPErrors";
+import DataController from "../DataController";
 
 interface Endpoint {
   data: unknown;
   headers: unknown;
 }
 
-type Request = (options: CreatedRequestOptions) => Promise<Endpoint>;
+type Request = (options?: CreatedRequestOptions) => Promise<Endpoint>;
 
 const createEndpoint = (type: RequestType, endpoint: string): Request => {
   const data = DataController.getInstance();
 
-  return async (options: CreatedRequestOptions): Promise<Endpoint> => {
-    const {
-      body,
-      pathParameters,
-      query,
-    } = options;
+  return async (options: CreatedRequestOptions = {}): Promise<Endpoint> => {
+    const { body, pathParameters, query } = options;
     let parsedEndpoint: string = null;
     let response: Response<string> = null;
 
-    if (pathParameters)
-      parsedEndpoint = parseEndpoint(endpoint, pathParameters);
+    // if (pathParameters)
+    parsedEndpoint = parseEndpoint(endpoint, pathParameters);
 
     if (query) {
-      if (query['api_key'])
-        delete query['api_key'];
-      if (query['language'])
-        delete query['language'];
+      if (query["api_key"]) delete query["api_key"];
+      if (query["language"]) delete query["language"];
     }
 
     const readyQuery = new URLSearchParams({
-      api_key: data.get('apiKey'),
-      language: data.get('language'),
+      api_key: data.get("apiKey"),
+      language: data.get("language"),
     });
 
     if (query) {
-      normalizeQuery(query).forEach(entry => {
+      normalizeQuery(query).forEach((entry) => {
         readyQuery.append(entry[0], entry[1]);
       });
     }
 
     try {
       switch (type) {
-        case 'GET': {
-          response = await request(parsedEndpoint, { searchParams: readyQuery });
+        case "GET": {
+          response = await request(parsedEndpoint, {
+            searchParams: readyQuery,
+          });
           break;
         }
 
-        case 'POST': {
+        case "POST": {
           response = await request.post(endpoint, {
             searchParams: readyQuery,
             json: body,
@@ -71,7 +67,7 @@ const createEndpoint = (type: RequestType, endpoint: string): Request => {
           break;
         }
 
-        case 'DELETE': {
+        case "DELETE": {
           response = await request.delete(endpoint, {
             searchParams: readyQuery,
             json: body,
@@ -94,7 +90,7 @@ const createEndpoint = (type: RequestType, endpoint: string): Request => {
 export default (endpoints: RequestOption[]): { [key: string]: Request } => {
   const root: { [key: string]: Request } = {};
 
-  endpoints.forEach(endpoint => {
+  endpoints.forEach((endpoint) => {
     root[endpoint.name] = createEndpoint(endpoint.type, endpoint.endpoint);
   });
 
